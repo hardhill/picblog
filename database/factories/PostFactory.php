fbed6a519f62cb10aeb6748c3f8287f5702b1004
hardhill <hardhill@mail.ru>
2021-07-19 19:26:26 +0800

    first commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->realText(rand(10,40));
+        $short_title = mb_strlen($title)>30?mb_substr($title,0,30).'...':$title;
+        $img = "https://via.placeholder.com/400/5611A8/C0C0C0/?text=Picture";
+        $created = $this->faker->dateTimeBetween('-1 years','-1 days');
+
+        return [
+            'title'=> $title,
+            'short_title'=>$short_title,
+            'description'=> $this->faker->realText(rand(100,500)),
+            'img'=>$img,
+            'created_at'=>$created,
+            'updated_at'=>$created
+        ];
+    }
+}
